62c4d3e2555619db0c0b3a4d1abc4c0f486b87ab
lucasfernandes <lucas.fernandes.silveira@gmail.com>
2021-12-26 18:39:43 -0300

    layout improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,14 @@ import { Keypair, SystemProgram, Transaction, PublicKey, LAMPORTS_PER_SOL } from
 import { useState, useEffect, useCallback } from 'react'
 
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
 
 const Home: NextPage = () => {
   const { connection } = useConnection()
   const { publicKey, sendTransaction } = useWallet()
   const [message, setMessage] = useState("")
   const [transactionSignature, setTransactionSignature] = useState("")
+
+  const img = "https://cdn.publish0x.com/prod/fs/images/367f59c20e8257c5b8cc9943a8a5fa1c9f977cac02e39268afa9c1d11b465c2d.jpg"
 
   const sendSol = useCallback(async () => {
     if (!publicKey) throw new WalletNotConnectedError();
@@ -53,8 +51,13 @@ const Home: NextPage = () => {
 
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen space-y-10 bg-gray-800'>
-      <div className='flex items-center justify-center space-x-2'>
+    <div className='flex flex-col items-center justify-end w-full h-screen min-h-screen pb-16 space-y-20'
+    style={{
+      backgroundSize: "cover",
+      backgroundAttachment: "fixed",
+      backgroundImage: `url(${img})`,
+    }}>
+      <div className='flex items-center justify-center space-x-4'>
         <WalletMultiButton />
         <button onClick={sendSol} disabled={!publicKey} className='px-6 py-3 font-bold text-white bg-red-400 rounded disabled:opacity-40 hover:opacity-75'>
           Send 1 SOL
@@ -66,7 +69,7 @@ const Home: NextPage = () => {
       </div>
       
       { message && 
-        <div className='px-4 py-2 text-xs font-bold text-center text-white bg-green-400 rounded'>
+        <div className='px-4 py-2 text-xs font-bold text-center text-white bg-purple-400 rounded'>
           {message} {transactionSignature && <a href={`https://solscan.io/tx/${transactionSignature}?cluster=devnet`} target="blank">- [Solscan]</a>}
         </div> }
     </div>
